ed224beb1510042adb5b70565a75a62f5f5f77c4
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2021-04-19 10:28:58 -0300

    Rutas listas

---
+++ src/components/productos/ListaDeProductos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListaDeProductos = () => {
+    return (
+        <div>
+            <h1>Página lista de productos</h1>
+        </div>
+    );
+};
+
+export default ListaDeProductos;
